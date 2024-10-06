daac453ea6808abd751aebada85106a52320e962
Shahboz <83070485+Shahboz007@users.noreply.github.com>
2024-10-06 20:59:06 +0500

    done add action and change todo item checked style

--- controller/main.controller.js
+++ controller/main.controller.js
@@ -1,16 +1,35 @@
 const tasksData = [];
+/* 
+    id: number
+    title: string
+    desc: string
+    status: pending or completed
+*/
 
 exports.getTasksPage = (req, res) => {
   return res.render("index", {
     title: "Tasks",
+    pendingTasks: tasksData.filter(task => task.status === 'pending'),
+    completedTasks: tasksData.filter(task => task.status === 'completed'),
   });
 };
 exports.getNewTaskPage = (req, res) => {
-  return res.render('todo-add',{
-    title: "Add a new task"
-  })
+  return res.render("todo-add", {
+    title: "Add a new task",
+  });
 };
-exports.createNewTask = (req, res) => {};
+exports.createNewTask = (req, res) => {
+  const { title, desc } = req.body;
+
+  tasksData.push({
+    id: tasksData.length > 0 ? tasksData[tasksData.length - 1] + 1 : 1,
+    title,
+    desc,
+    status: "pending",
+  });
+
+  return res.redirect("/");
+};
 exports.getEditTaskPage = (req, res) => {};
 exports.updateTask = (req, res) => {};
 exports.deleteTask = (req, res) => {};
